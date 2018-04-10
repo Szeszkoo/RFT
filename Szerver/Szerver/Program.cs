c4a875ae393da4b7e7c92192751ffca8f4e2f207
Dani <dancsiko@gmail.com>
2018-04-10 14:25:36 +0200

    Server-Klient connection and communication

--- Szerver/Szerver/Program.cs
+++ Szerver/Szerver/Program.cs
@@ -1,15 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Szerver
 {
+    class Protokoll
+    {
+        public StreamReader r;
+        public StreamWriter w;
+        public string user = null;
+
+        public Protokoll(TcpClient c)
+        {
+            this.r = new StreamReader(c.GetStream(), Encoding.UTF8);
+            this.w = new StreamWriter(c.GetStream(), Encoding.UTF8);
+        }
+
+        public void StartKomm()
+        {
+            w.WriteLine("Akció 1.0 béta");
+            w.Flush();
+            bool ok = true;
+            while (ok)
+            {
+                string parancs = null;
+                try
+                {
+                    string uzenet = r.ReadLine();
+                    string[] param = uzenet.Split('|');
+                    parancs = param[0].ToUpper();
+                    switch (parancs)
+                    {
+                        //függvények érkeznek majd ide
+                        case "BYE": w.WriteLine("BYE"); ok = false; break;
+                        default: w.WriteLine("ERR|Ismeretlen parancs"); break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    w.WriteLine("ERR|{0}", e.Message);
+                }
+                w.Flush();
+            }
+            Console.WriteLine("A kliens elköszönt");
+        }
+    }
+
+
     class Program
     {
+        const int portSzam = 54325;
+        const string ipcim = "127.0.0.1";
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Szerver elindult");
+            try
+            {
+                IPAddress ip = IPAddress.Parse(ipcim);
+                TcpListener k = new TcpListener(ip, portSzam);
+                k.Start();
+                while (true)
+                {
+                    Console.WriteLine("A szerver bejövő kapcsolatra vár");
+                    TcpClient client = k.AcceptTcpClient();
+                    Console.WriteLine("Valaki jött, akar vmit!");
+                    Protokoll p = new Protokoll(client);
+                    Thread t = new Thread(p.StartKomm);
+                    t.Start();
+                }
+            }
+            catch { Console.WriteLine("Nem indult el"); }
+            Console.ReadLine();
         }
     }
 }
